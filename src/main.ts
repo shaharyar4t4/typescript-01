15a69815ddfe76a360ba17fff72580da4b02429f
Shaharyar <shaharyar4t4@gmail.com>
2025-07-15 00:40:35 +0500

    about array behave on typscritp

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,14 @@ console.log('Hello, World!');
 
 
 // this is show me variable is take the number or string both thing
-var name: (string | number) = "shaharyar";
-console.log(name);
+// var name: (string | number) = "shaharyar";
+// console.log(name);
+
+// this is show the array of string and number
+// var arr:(string | number)[]= [1, 2, 3, 5, 6];
+// arr.push("11");
+// arr.push(9);
+// console.log(arr);       
+
+
+
