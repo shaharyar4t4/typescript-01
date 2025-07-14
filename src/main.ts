3aebfb47e43508d310073c39909888f07a1e8bd3
Shaharyar <shaharyar4t4@gmail.com>
2025-07-15 01:08:37 +0500

    updated typescript

--- src/main.ts
+++ src/main.ts
@@ -2,26 +2,26 @@ console.log('Hello, World!');
 
 
 // this is show me variable is take the number or string both thing
-// var name: (string | number) = "shaharyar";
-// console.log(name);
+var name: (string | number) = "shaharyar";
+console.log(name);
 
 // this is show the array of string and number
-// var arr:(string | number)[]= [1, 2, 3, 5, 6];
-// arr.push("11");
-// arr.push(9);
-// console.log(arr);       
+var arr:(string | number)[]= [1, 2, 3, 5, 6];
+arr.push("11");
+arr.push(9);
+console.log(arr);       
 
 // this is show how obj behave on typescript
-// let obj: {name: string, age:number, email?: string}={
-//   name: "shaharyar",
-//   age: 23,
-//   email: "",
-// }
-// obj.email="shaharyar@gmail.com"
-// console.log(obj);
+let obj: {name: string, age:number, email?: string}={
+  name: "shaharyar",
+  age: 23,
+  email: "",
+}
+obj.email="shaharyar@gmail.com"
+console.log(obj);
 
-const num1 = document.getElementById("inputOne")! as HTMLInputElement;
-const num2 = document.getElementById("inputTwo")! as HTMLInputElement;
+const num1 = document.getElementById("inputone")! as HTMLInputElement;
+const num2 = document.getElementById("inputtwo")! as HTMLInputElement;
 const btn = document.getElementById("btn")! as HTMLButtonElement;
 
 
